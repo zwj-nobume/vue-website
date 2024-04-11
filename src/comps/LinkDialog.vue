d3bb4a800f7e42da76c977c28fa9580007d80875
nobume <zwj-nobume@outlook.com>
2024-04-11 20:05:00 +0800

    linkdialog

---
+++ src/comps/LinkDialog.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    url: {
+        type: Object,
+        required: true
+    },
+    allData: {  // 所有数据
+        type: Array,
+        required: true,
+    },
+    avaiData: { // 已有数据
+        type: Set,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in allData" :key="item" :data-name="item.name">{{ item.label }}</li>
+        <LinkDialog v-if="item?.children?.length"></LinkDialog>
+    </ul>
+</template>
+
+<style scoped></style>
