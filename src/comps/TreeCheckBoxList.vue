fb0afce5c6e744a83798e52756cb7fa85f139574
zwj-nobume <zwj-nobume@outlook.com>
2024-07-20 22:56:30 +0800

    树形多选框选中或取消选中时同步到子元素

--- src/comps/TreeCheckBoxList.vue
+++ src/comps/TreeCheckBoxList.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { isEmpty } from '@/utils/public';
+import { toRaw } from 'vue';
+
 const props = defineProps({
 	elems: {
 		type: Array,
@@ -6,13 +9,22 @@ const props = defineProps({
 	},
 })
 
+const boxChange = (event) => {
+	const id = event.target.value
+	const idIndex = toRaw(props.elems).map(item => item.id).indexOf(id)
+	addAll(props.elems[idIndex].children, event.target.checked)
+}
+const addAll = (items = [], flag = true) => items.forEach(item => {
+	flag ? model.value.add(item.id) : model.value.delete(item.id)
+	if (!isEmpty(item.children)) addAll(item.children, flag)
+})
 const model = defineModel()
 </script>
 
 <template>
 	<ul>
 		<li v-for="elem in elems">
-			<input type="checkbox" :value="elem.id" v-model="model" />
+			<input type="checkbox" :value="elem.id" v-model="model" @change="boxChange" />
 			<label>{{ elem.name }}</label>
 			<TreeCheckBoxList v-if="elem.children" :elems="elem.children" v-model="model"></TreeCheckBoxList>
 		</li>
