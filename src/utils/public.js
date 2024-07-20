de3f053a1375a49144d751617389286541fc7e35
zwj-nobume <zwj-nobume@outlook.com>
2024-07-20 22:54:12 +0800

    添加数组转树形结构

--- src/utils/public.js
+++ src/utils/public.js
@@ -13,4 +13,23 @@ const isBlank = (item) => {
     return isNull(item) || ((typeof item === 'string' || item instanceof String) && item == '')
 }
 
-export { isBlank, isEmpty, isNull }
+const listToTree = (data, keyName = 'id', parentName = 'parentId', childName = 'children') => {
+    if (isEmpty(data)) return []
+    const tree = []
+    const tempMap = new Map()
+    data.forEach(item => {
+        tempMap.set(item[keyName], item)
+        if (!isBlank(item[parentName]) && tempMap.has(item[parentName])) {
+            const parentItem = tempMap.get(item[parentName])
+            if (isNull(parentItem[childName])) {
+                parentItem[childName] = new Array()
+            }
+            parentItem[childName].push(item)
+        } else {
+            tree.push(item)
+        }
+    })
+    return tree
+}
+
+export { isBlank, isEmpty, isNull, listToTree }
